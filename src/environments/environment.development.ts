2a292085c17afd0a1ea4f1f05e918d15fd1f2ffc
Théo Lemray <98155180+getill@users.noreply.github.com>
2025-05-14 15:10:48 +0200

    trying to get a callback

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,10 @@ export const environment = {
   production: false,
   clientId: '9dcab34078bc416e859ed8a2657c7d8c',
   redirectUri: 'http://localhost:4200/callback',
+  scopes: [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'playlist-read-private',
+  ].join(' '),
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
   clientId: '9dcab34078bc416e859ed8a2657c7d8c',
-  redirectUri: 'https://wpm-one.vercel.app/callback',
+  redirectUri: 'http://localhost:4200/callback',
+  scopes: [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+    'playlist-read-private',
+  ].join(' '),
 };

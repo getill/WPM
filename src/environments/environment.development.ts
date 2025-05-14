d8877d669506a171a0d81e7f150734e7bd68b226
Théo Lemray <98155180+getill@users.noreply.github.com>
2025-05-14 12:15:12 +0200

    Environement added

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  spotifyToken:
+    'BQD1nLNXzTmdHbIbf_wGRKaIiLkir9GbjWmWOJ_Hzo64CVyPSoOeOkddoFARBNIGl5jIJpM63Mvj6qqFIPrpGqWJXZmFD-zh4lC7TxCoRaHxdjxOgZDcqKqlRhDrJaTeurxoA7V5AjQ',
+};

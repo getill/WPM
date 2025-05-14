8390ad4b451acbfa2755b49b342a2c26c48bbc72
Théo Lemray <98155180+getill@users.noreply.github.com>
2025-05-14 15:13:34 +0200

    trying to get a callback

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   clientId: '9dcab34078bc416e859ed8a2657c7d8c',
-  redirectUri: 'https://wpm-one.vercel.app/callback',
+  redirectUri: 'https://wpm-one.vercel.app/callback/',
   scopes: [
     'user-read-private',
     'user-read-email',

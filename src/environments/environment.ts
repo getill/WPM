7a0b39155fc659bf23afdd235030a24cead27ad9
Théo Lemray <98155180+getill@users.noreply.github.com>
2025-05-14 14:44:09 +0200

    Callback fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   clientId: '9dcab34078bc416e859ed8a2657c7d8c', // From Spotify Developer Dashboard
-  redirectUri: 'http://localhost:4200/callback',
+  redirectUri: 'https://wpm-one.vercel.app/callback',
 };

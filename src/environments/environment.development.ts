a9f339b4107783c393ec253884a2151aba69033c
Théo Lemray <98155180+getill@users.noreply.github.com>
2025-06-10 13:08:19 +0200

    Gif added to readme

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   spotifyToken:
-    'BQBRQAvACdd_b8GyoVSL4PCklOAlt_1C5eM-8iY5U6U0oK0RiD07odJ_hcNS1UyyXM1WoghuybcoElk7twmalOq7M3q-3Nu0U_tcBMOipWJzG0aDWimSDuX18SFOhMMBJcFgpYj4x1o',
+    'BQA_6jP1kY05rhnveWWJ8_y2XEGwohmQnOfAX7sJrIPU0UGPftYxtxtDQCwdKEVvFNDEGSvvQ82vysQOoTPuno3MufhK5M918VdPzCyU7rptebnBVS5YUezp1HH5F6Uytoq7V95jUAs',
 };
